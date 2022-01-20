dadf6ef2a72a169f0cd0283468d32f0450865e53
Laura <laura.de.jong@nobleo.nl>
2022-01-20 14:56:55 +0100

    fixed Chatbot Constructor with memory alloc repeat call

--- src/graphnode.cpp
+++ src/graphnode.cpp
@@ -37,13 +37,15 @@ void GraphNode::AddEdgeToChildNode(GraphEdge *edge)
 //// changed
 void GraphNode::MoveChatbotHere(ChatBot chatbot)
 {
-    _chatBot = std::move(chatbot);
-    _chatBot.SetCurrentNode(this);
+    std::cout << "MoveChatbotNewnode" << std::endl;
+    *_chatBot = std::move(chatbot);
+    _chatBot->SetCurrentNode(this);
 }
 
 void GraphNode::MoveChatbotToNewNode(GraphNode *newNode)
 {
-    newNode->MoveChatbotHere(std::move(_chatBot));
+    std::cout << "MoveChatbotNewnode" << std::endl;
+    newNode->MoveChatbotHere(std::move(*_chatBot));
     // _chatBot = nullptr; // invalidate pointer at source
 }
 ////
